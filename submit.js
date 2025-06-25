5a2fa8fea94b132fd093c8ce94ab4eb3f6ef2812
kobbynicholas <159589377+kobbynicholas@users.noreply.github.com>
2025-06-26 00:48:14 +0100

    Update submit.js

--- submit.js
+++ submit.js
@@ -1,1 +1,22 @@
+const scriptURL = ''
 
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! marks submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
+
+function showDiv() {
+  document.getElementById('submit').style.display = "flex";
+  document.getElementById('loadingGif').style.display = "flex";
+  setTimeout(function() {
+    document.getElementById('loadingGif').style.display = "flex";
+    document.getElementById('showme').style.display = "flex";
+  },5000);
+   
+}
