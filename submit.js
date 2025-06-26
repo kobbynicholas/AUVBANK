9590520bb4a895859d269bf654fef6395f123775
kobbynicholas <159589377+kobbynicholas@users.noreply.github.com>
2025-06-26 23:30:32 +0100

    Update submit.js

--- submit.js
+++ submit.js
@@ -3,10 +3,10 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbx1MotN-FVnVdEeZQf5OO
 
 const form = document.forms['contact-form']
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit', e => { 
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! marks submitted successfully." ))
+  .then(response => alert("Thank you! submitted successfully." ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
